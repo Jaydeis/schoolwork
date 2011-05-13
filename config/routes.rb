4cc8e61cfee0a070d22408d8f246f990b1cc1881
jaydeis <moonlight_anime@yahoo.com>
2011-05-13 05:08:13 -0400

    Bluh bluh

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Schoolwork::Application.routes.draw do
+  get "pages/home"
+  get "pages/contact"
+  get "pages/about"
+  get "pages/help"
   get "users/new"
 
   match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
 
 
   #get "welcome/index"
 
-  resources :microposts
+  #resources :microposts
 
-  resources :users
+  #resources :users
 
    root :to => "pages#home"
 end
